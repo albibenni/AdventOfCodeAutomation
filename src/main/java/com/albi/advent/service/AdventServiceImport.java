118feafcada7a0dd29a525f66531018d98cf0e46
Alberto Benatti <albi.benni8@gmail.com>
2022-12-06 08:48:54 +0100

    WIP - 400 error response Check header

--- src/main/java/com/albi/advent/service/AdventServiceImport.java
+++ src/main/java/com/albi/advent/service/AdventServiceImport.java
@@ -2,11 +2,12 @@ package com.albi.advent.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.List;
 
 @Service
@@ -18,10 +19,69 @@ public class AdventServiceImport {
         return List.of("el1", year.toString(), day.toString());
     }
 
-    private List<String> fetchAdventURLBasedOnYearAndDay(Integer year, Integer day) throws IOException {
-        URLConnection urlConnection = new URL("https://adventofcode.com/2022/day/1/input").openConnection();
-//        String response = urlConnection.getResponseMessage();
-//        System.out.println(response);
+    private List<String> fetchAdventURLBasedOnYearAndDay(Integer year, Integer day) {
+
+        //  Step 1:  Start creating a few objects we'll need.
+        URL url;
+        InputStream is = null;
+        DataInputStream dis;
+        String s;
+        try {
+            // Step 2:  Create the URL.
+            //------------------------------------------------------------//
+            // Note: Put your real URL here, or better yet, read it as a
+            // command-line arg, or read it from a file.
+            url = new URL("https://adventofcode.com/2022/day/1/input");
+
+            // Step 3:  Open an input stream from the url.
+            is = url.openStream(); //IO exception
+
+
+            // Step 4:                                                     //
+            //-------------------------------------------------------------//
+            // Convert the InputStream to a buffered DataInputStream.      //
+            // Buffering the stream makes the reading faster; the          //
+            // readLine() method of the DataInputStream makes the reading  //
+            // easier.                                                     //
+            //-------------------------------------------------------------//
+
+            dis = new DataInputStream(new BufferedInputStream(is));
+            // Step 5:                                                    //
+            //------------------------------------------------------------//
+            // Now just read each record of the input stream, and print   //
+            // it out.  Note that it's assumed that this problem is run   //
+            // from a command-line, not from an application or applet.    //
+            //------------------------------------------------------------//
+
+            while ((s = dis.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (MalformedURLException mue) {
+
+            System.out.println("MalformedURLException: " + mue);
+            mue.printStackTrace();
+            System.exit(1);
+
+        } catch (IOException ioe) {
+
+            System.out.println("IOException: "+ioe);
+            ioe.printStackTrace();
+            System.exit(1);
+
+        } finally {
+
+            //---------------------------------//
+            // Step 6:  Close the InputStream  //
+            //---------------------------------//
+
+            try {
+                is.close();
+            } catch (IOException ioe) {
+
+            }
+        }
         return null;
     }
+
+
 }
